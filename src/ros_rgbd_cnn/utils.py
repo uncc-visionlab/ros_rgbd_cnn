da2db53aa24ae5c61d58175a078f64a134f50652
Jincheng Zhang <mambainustc@gmail.com>
2020-12-12 11:50:18 -0500

    Update utils.py

--- src/ros_rgbd_cnn/utils.py
+++ src/ros_rgbd_cnn/utils.py
@@ -2,6 +2,10 @@ import numpy as np
 from torch import nn
 import torch
 import os
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+import imageio
 import sys
 from six.moves.urllib import request
 import shutil
@@ -48,43 +52,44 @@ label_colours = [(0, 0, 0),
 
 '''
 #An example of using depth2plane:
-intrinsicPath = './data/SUNRGBD/kv1/NYUdata/NYU0002/intrinsics.txt'
+intrinsicPath = '$dataPath$/data/SUNRGBD/kv1/NYUdata/NYU0002/intrinsics.txt'
 fittingSize = 2
-extrinsicPath = './data/SUNRGBD/kv1/NYUdata/NYU0002/extrinsics/20150118235913.txt'
+extrinsicPath = '$dataPath$/data/SUNRGBD/kv1/NYUdata/NYU0002/extrinsics/20150118235913.txt'
 import imageio
-from utils.utils import depth2plane
-depth = imageio.imread('./data/SUNRGBD/kv1/NYUdata/NYU0002/depth_bfx/NYU0002.png')
-labelPath = './data/SUNRGBD/kv1/NYUdata/NYU0002/label/label.npy'
-plane = depth2plane(depth, extrinsicPath, intrinsicPath, labelPath, fittingSize)
+from utils import depth2plane
+depth = imageio.imread('$dataPath$/data/SUNRGBD/kv1/NYUdata/NYU0002/depth_bfx/NYU0002.png')
+#labelPath = '$dataPath$/data/SUNRGBD/kv1/NYUdata/NYU0002/label/label.npy'
+plane = depth2plane(depth, extrinsicPath, intrinsicPath, fittingSize)
 newLabel = plane.getPlaneLabel()
 planeImage = plane.getPlaneImage()
-plane.visualizePlaneImage(planeImage)
-rgbPath = './data/SUNRGBD/kv1/NYUdata/NYU0002/image/NYU0002.jpg'
-plane.visualizePointCloud(rgbPath)
+#plane.visualizePlaneImage(planeImage)
+#rgbPath = '$dataPath$/data/SUNRGBD/kv1/NYUdata/NYU0002/image/NYU0002.jpg'
+#plane.visualizePointCloud(rgbPath)
 '''
 
 
 class depth2plane:
-    def __init__(self, depth, extrinsicPath, intrinsicPath, labelPath, fittingSize=5):
+    def __init__(self, depth, extrinsicPath, intrinsicPath, fittingSize=5):  # add labelPath as one argument if needed
         self.depthImage = depth
         self.extrinsicPath = extrinsicPath
         self.intrinsicPath = intrinsicPath
         self.fittingSize = fittingSize
-        self.labelPath = labelPath
+        #self.labelPath = labelPath
 
     def getPlaneImage(self):
         planeImage = self.estimate_planes()
-        planeImage = self.smoothing(planeImage)
         return planeImage
 
     def getPlaneLabel(self):
         label = np.load(self.labelPath)
         winsize = self.fittingSize
-        halfwinsize = max(1, int(0.5 * (winsize - 1)))
+        #halfwinsize = int(0.5 * winsize)
         newLabel = np.zeros(shape=(int(label.shape[0] / winsize), int(label.shape[1] / winsize)))
-        for y in range(halfwinsize, label.shape[0] - halfwinsize, winsize):
-            for x in range(halfwinsize, label.shape[1] - halfwinsize, winsize):
-                windowLabels = label[(y - halfwinsize):(y + halfwinsize + 1), (x - halfwinsize):(x + halfwinsize + 1)]
+        #for y in range(halfwinsize, label.shape[0] - halfwinsize, winsize):
+        for y in range(0, label.shape[0]-winsize, winsize):
+            for x in range(0, label.shape[1]-winsize, winsize):
+                windowLabels = label[y:(y + winsize + 1), x:(x + winsize + 1)]
+
                 newLabel[int(y / winsize), int(x / winsize)] = np.max(windowLabels)
         return newLabel
 
@@ -151,12 +156,12 @@ class depth2plane:
         #depthImage = self.smoothing(depthImage, filtersize=3)  # smoothing the inpainted depth image will cause bad fitting result
         depth_img = self.bitShiftDepthMap(depthImage)
         # size of window is 2*halfwinsize+1
-        halfwinsize = max(1, int(0.5 * (winsize - 1)))
         [ptCloud, _] = self.depthImage2ptcloud(depth_img)
+        #ptCloud = self.smoothing(ptCloud, filtersize=5)
         planes_img = np.zeros(shape=(int(depth_img.shape[0] / winsize), int(depth_img.shape[1] / winsize), 4), dtype=np.float32)
-        for y in range(halfwinsize, depth_img.shape[0] - halfwinsize, winsize):
-            for x in range(halfwinsize, depth_img.shape[1] - halfwinsize, winsize):
-                windowDepths = depth_img[(y - halfwinsize):(y + halfwinsize + 1), (x - halfwinsize):(x + halfwinsize + 1)]
+        for y in range(0, depth_img.shape[0]-winsize, winsize):
+            for x in range(0, depth_img.shape[1]-winsize, winsize):
+                windowDepths = depth_img[y:(y + winsize + 1), x:(x + winsize + 1)]
                 # print(windowDepths)
                 numValidPoints = np.count_nonzero(~np.isnan(windowDepths))
                 # print(numValidPoints)
@@ -166,8 +171,8 @@ class depth2plane:
                     pts3D = np.empty(shape=(numValidPoints, 3), dtype=np.float32)
                     offset = 0
                     # print(pts3D)
-                    for ywin in range(-halfwinsize, halfwinsize + 1):
-                        for xwin in range(-halfwinsize, halfwinsize + 1):
+                    for ywin in range(0, winsize + 1):
+                        for xwin in range(0, winsize + 1):
                             if (~np.isnan(ptCloud[y + ywin, x + xwin, 2])):
                                 pts3D[offset, :] = ptCloud[y + ywin, x + xwin, :]
                                 offset += 1
@@ -180,26 +185,30 @@ class depth2plane:
         plane3 = np.empty(shape=(4), dtype=np.float32)
         centroid = np.mean(points, 0)
         demeaned_pts3D = points - centroid
-        _MtM = np.dot(demeaned_pts3D.transpose(), demeaned_pts3D)
+        #_MtM = np.dot(demeaned_pts3D.transpose(), demeaned_pts3D)
+        _MtM = np.matmul(demeaned_pts3D.transpose(), demeaned_pts3D)
         [_, v] = LA.linalg.eigh(_MtM)
         plane3[0:3] = v[:, 0]
         plane3[3] = -np.dot(plane3[0:3], centroid[:])
-        #if (plane3[2] > 0):
-        #    plane3 = -plane3   # might not be necessary since we flipped y-axis when generating the point cloud
+        if (plane3[2] > 0):
+            plane3 = -plane3
         return plane3
 
-    def smoothing(self, img, filtersize=11):
+    def smoothing(self, img, filtersize=3):
         img = cv2.GaussianBlur(img, (filtersize, filtersize), cv2.BORDER_CONSTANT)
         return img
 
     def visualizePlaneImage(self, plane_img):
-        imageio.imwrite('0002.tiff', plane_img)
-        #cv2.namedWindow("planeImage", cv2.WINDOW_NORMAL)
-        cv2.imshow("planeImage", plane_img[:, :, 0:3])
-        cv2.imshow("a", plane_img[:, :, 0])
-        cv2.imshow("b", plane_img[:, :, 1])
-        cv2.imshow("c", plane_img[:, :, 2])
-        cv2.imshow("d", plane_img[:, :, 3])
+        vis_plane_img_a = np.zeros(shape=(plane_img.shape[0], plane_img.shape[1], 3), dtype=np.uint8)
+        vis_plane_img_b = np.zeros(shape=(plane_img.shape[0], plane_img.shape[1], 3), dtype=np.uint8)
+        vis_plane_img_c = np.zeros(shape=(plane_img.shape[0], plane_img.shape[1], 3), dtype=np.uint8)
+        vis_planes_img_d = np.zeros(shape=(plane_img.shape[0], plane_img.shape[1], 3), dtype=np.uint8)
+        for y in range(0, plane_img.shape[0]):
+            for x in range(0, plane_img.shape[1]):
+                vis_plane_img_c[y, x, :] = plane_img[y, x, 2] * 255
+        cv2.imshow("c", vis_plane_img_c)
+        cv2.imshow("planeImage", abs(plane_img[:,:,0:3]))
+        cv2.imshow("abs_planeImage", plane_img[:,:, 0:3])
         cv2.waitKey()
 
     def visualizePointCloud(self, rgbpath):
